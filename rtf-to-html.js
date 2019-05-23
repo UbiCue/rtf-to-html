5dc6bdb14c14744c3a2a516acde3798b9371464b
Edmund LoPresti <edlopresti@ubicueinc.com>
2019-05-23 13:59:03 -0400

    Update rtf-to-html.js

--- rtf-to-html.js
+++ rtf-to-html.js
@@ -91,7 +91,7 @@ function paragraphStyles(chunk, defaults) {
 	css += `text-indent: ${chunk.style.firstLineIndent / 20}pt;`
    }
    if (chunk.style.indent != null && chunk.style.indent !== undefined && chunk.style.indent !== defaults.indent) {
-   	css += `padding-left: ${12+(chunk.style.indent / 20)}pt;`
+   	css += `padding-left: ${chunk.style.indent / 20}pt;`
    }
    if (chunk.style.align != null && chunk.style.align !== undefined && chunk.style.align !== defaults.align) {
     	css += `text-align: ${chunk.style.align};`
